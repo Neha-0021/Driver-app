b1a1f05ad2cd2d4054fa2a78d210f05ad72bc2bc
Neha-0021 <nehasingh2002rgh@gmail.com>
2023-07-21 10:44:56 +0530

    commit

--- lib/Molecules/Map.dart
+++ lib/Molecules/Map.dart
@@ -15,33 +15,36 @@ class Map extends StatefulWidget {
 class _MapState extends State<Map> {
   String htmlContent() {
     return '''
-    <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Display a map with a custom style</title>
-    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
-    <link href="https://unpkg.com/mapbox-gl@2.4.1/dist/mapbox-gl.css" rel="stylesheet" />
-    <style>
-      body { margin: 0; padding: 0; }
-      #map { position: absolute; top: 0; bottom: 0; width: 100%; }
-    </style>
-  </head>
-  <body>
-    <div id="map"></div>
-    <script src="https://maps.flightmap.io/flightmapjs"></script>
-    <script>
-      var map = new flightmap.Map({
-        container: 'map', // container id
-        style: 'style-dark.json', // hosted style id
-        center: [-77.38, 39], // starting position
-        zoom: 3, // starting zoom
-        accessToken: '88108930-b699-11ed-8a6d-8f496b2a9392'
-      });
-    </script>
-  </body>
-</html>
+<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <title>Display a map with a custom style</title>
+        <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
+        <link href="https://unpkg.com/mapbox-gl@2.4.1/dist/mapbox-gl.css" rel="stylesheet" />
+        <style>
+          body { margin: 0; padding: 0; }
+          #map { position: absolute; top: 0; bottom: 0; width: 100%; }
+        </style>
+      </head>
+      <body>
+        <div id="map"></div>
+        <script src="https://maps.flightmap.io/flightmapjs"></script>
+        <script>
+          var map = new flightmap.Map({
+            container: 'map', // container id
+            style: 'style-dark.json',
+            center: [-77.38, 39], 
+            zoom: 3, 
+            accessToken: '88108930-b699-11ed-8a6d-8f496b2a9392'
+          });
 
+          var marker = new flightmap.Marker()
+            .setLngLat([-77.38, 39])
+            .addTo(map);
+        </script>
+      </body>
+    </html>
 ''';
   }
 
