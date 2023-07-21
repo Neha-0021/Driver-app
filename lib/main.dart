9e7e895659e7cfaecf0f3929f259eda062a0c6be
neha pandey <nehapandey4495@gmail.com>
2023-07-21 17:49:15 +0530

    commit message

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:driver_app/Pages/Notification-page.dart';
 import 'package:driver_app/Pages/Driver/DriverRating.dart';
 import 'package:driver_app/Pages/Profile/personal-details-page.dart';
 import 'package:driver_app/Pages/login.dart';
+import 'package:driver_app/atom/BackgroundFetch/get_lat_long_address.dart';
 import 'package:driver_app/state-management/home-state.dart';
 import 'package:driver_app/state-management/notification-state.dart';
 import 'package:driver_app/state-management/profile-state.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
         ],
         child: MaterialApp(
           title: 'DRIVER',
-          initialRoute: 'HomePage',
+          initialRoute: 'login',
           routes: {
             'personal-details-page': (context) => PersonalDetailPage(),
             'DriverRating': (context) => const DriverRating(),
@@ -40,6 +41,7 @@ class MyApp extends StatelessWidget {
             'Next-stop': (context) => const NextStep(),
             'HomePage': (context) => const HomePage(),
             'bottom-tabbar': (context) => const BottomBar(),
+            
           },
         ));
   }
